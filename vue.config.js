631ea6cc266622cf6b8b5c302c2c2607c6f76e50
dada <1262442109@qq.com>
2023-04-06 16:29:09 +0800

    🐎 ci: 更新publicPath

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  publicPath: './',
   // chainWebpack: (config) => {
   //   config.resolve.alias.set('@', path.resolve(__dirname, './src'));
   // },
